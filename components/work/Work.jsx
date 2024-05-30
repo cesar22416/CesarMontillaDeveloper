26a67b6d6a44d0dada337c9f0a9c99dd231d6fb1
Cesar Montilla <115080014+cesar22416@users.noreply.github.com>
2024-05-30 13:18:27 -0300

    version0.1

--- components/work/Work.jsx
+++ components/work/Work.jsx
@@ -17,7 +17,7 @@ const Work = () => {
             
             
             <h1 className='titulowork'>Motorcycle trip app</h1>
-            <Link to="/public/pdf/APPmot.pdf" target="_blank"> <CircularButton  color="#FAFF01" icon="https://firebasestorage.googleapis.com/v0/b/cesar22.appspot.com/o/flecha.png?alt=media&token=f3eb9225-567b-473b-a140-9f597a54a569"/></Link>
+            <Link to="https://firebasestorage.googleapis.com/v0/b/cesar22.appspot.com/o/APPmot.pdf?alt=media&token=7bdd8dec-7300-47eb-a550-e0271db4cd42" target="_blank"> <CircularButton  color="#FAFF01" icon="https://firebasestorage.googleapis.com/v0/b/cesar22.appspot.com/o/flecha.png?alt=media&token=f3eb9225-567b-473b-a140-9f597a54a569"/></Link>
             </div>
             <div className='hrbase'>
               <p className='worlkt'>Design UX/UI</p>
@@ -36,7 +36,7 @@ const Work = () => {
             
             
             <h1>Social networking</h1>
-            <Link to="/public/pdf/app.pdf" target="_blank"> <CircularButton  color="#FAFF01" icon="https://firebasestorage.googleapis.com/v0/b/cesar22.appspot.com/o/flecha.png?alt=media&token=f3eb9225-567b-473b-a140-9f597a54a569"/></Link>
+            <Link to="https://firebasestorage.googleapis.com/v0/b/cesar22.appspot.com/o/app.pdf?alt=media&token=95656506-7cb0-4ae3-825f-77a5270282f0" target="_blank"> <CircularButton  color="#FAFF01" icon="https://firebasestorage.googleapis.com/v0/b/cesar22.appspot.com/o/flecha.png?alt=media&token=f3eb9225-567b-473b-a140-9f597a54a569"/></Link>
             </div>
             <div className='hrbase'>
               <p className='worlkt'>Design UX/UI</p>
@@ -55,7 +55,7 @@ const Work = () => {
             
             
             <h1>Shoes app</h1>
-            <Link to="/public/pdf/shoes.pdf" target="_blank"><CircularButton  color="#FAFF01" icon="https://firebasestorage.googleapis.com/v0/b/cesar22.appspot.com/o/flecha.png?alt=media&token=f3eb9225-567b-473b-a140-9f597a54a569"  /></Link> 
+            <Link to="https://firebasestorage.googleapis.com/v0/b/cesar22.appspot.com/o/shoes.pdf?alt=media&token=0d994412-9574-407e-86ee-f50c60d8d383" target="_blank"><CircularButton  color="#FAFF01" icon="https://firebasestorage.googleapis.com/v0/b/cesar22.appspot.com/o/flecha.png?alt=media&token=f3eb9225-567b-473b-a140-9f597a54a569"  /></Link> 
             </div>
             <div className='hrbase'>
               <p className='worlkt'>Design UX/UI</p>
